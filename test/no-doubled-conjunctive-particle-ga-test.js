f919fd283a28890959f6579130449f816d004183
azu <azuciao@gmail.com>
2021-04-25 12:45:00 +0900

    fix(deps): update kuromojin@3 and sentence-splitter

--- test/no-doubled-conjunctive-particle-ga-test.js
+++ test/no-doubled-conjunctive-particle-ga-test.js
@@ -1,6 +1,7 @@
-import rule from "../src/no-doubled-conjunctive-particle-ga";
 import TextLintTester from "textlint-tester";
-var tester = new TextLintTester();
+import rule from '../src/no-doubled-conjunctive-particle-ga';
+
+const tester = new TextLintTester();
 tester.run("no-doubled-conjunctive-particle-ga", rule, {
     valid: [
         "この関数がエラーになるのは、関数名が正しくないためです。",
@@ -26,10 +27,12 @@ tester.run("no-doubled-conjunctive-particle-ga", rule, {
         },
         {
             text: "今日は早朝から出発したが，定刻には間に合わなかった．間に合わなかったが，無事会場に到着した．",
+            options: {
+                separatorChars: ["。"] // ． を除外
+            },
             errors: [
                 {
                     message: `文中に逆接の接続助詞 "が" が二回以上使われています。`,
-                    // last match
                     line: 1,
                     column: 12
                 }
