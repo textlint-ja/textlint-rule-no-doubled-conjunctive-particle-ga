778ebea71b36d8363e16f90edfeed8db220a3ee4
azu <azuciao@gmail.com>
2021-04-25 12:49:14 +0900

    test: add test case

--- test/no-doubled-conjunctive-particle-ga-test.js
+++ test/no-doubled-conjunctive-particle-ga-test.js
@@ -5,6 +5,7 @@ const tester = new TextLintTester();
 tester.run("no-doubled-conjunctive-particle-ga", rule, {
     valid: [
         "この関数がエラーになるのは、関数名が正しくないためです。",
+        "この文章が問題となっています。",
         "今日は早朝から出発したが、定刻には間に合わなかった。が、無事会場に到着した。",
         {
             text: "今日は早朝から出発したが，定刻には間に合わなかった．間に合わなかったが，無事会場に到着した．",
@@ -26,6 +27,16 @@ tester.run("no-doubled-conjunctive-particle-ga", rule, {
             ]
         },
         {
+            text: "規模は小さいが、収益は多いが、実益は小さい。",
+            errors: [
+                {
+                    message: `文中に逆接の接続助詞 "が" が二回以上使われています。`,
+                    index: 6
+                }
+            ]
+        },
+        // option test
+        {
             text: "今日は早朝から出発したが，定刻には間に合わなかった．間に合わなかったが，無事会場に到着した．",
             options: {
                 separatorChars: ["。"] // ． を除外
@@ -37,6 +48,6 @@ tester.run("no-doubled-conjunctive-particle-ga", rule, {
                     column: 12
                 }
             ]
-        }
+        },
     ]
 });
